7a7b0440fd7ae365d73ac3092f5c4d8396282383
tesla <tyrov1234@yandex.ru>
2026-01-08 00:58:17 +0500

    Негативный тест

--- chrome негативный тест.py
+++ chrome негативный тест.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 #настройки браузера Chrome
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
+#options.add_argument("--headless") #работа браузера в режиме headless
 
 #установка актуального драйвера для хрома и запуск самого браузера
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
@@ -23,7 +24,7 @@ print("input login") #печатаем в вывод
 
 #методом find_element с помощью XPATH и командой send_keys ищем строку пароля и вводим "secret_sauce"
 password = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input")
-password.send_keys("secret_sauce")
+password.send_keys("secret_sauc")
 print("input password") #печатаем в вывод
 
 #ищем елемент "логин" и командой "сlick" нажимаем на елемент
@@ -31,17 +32,29 @@ login_button = driver.find_element(By.ID, "login-button")
 login_button.click()
 print("click login button") #печатаем в вывод
 
-print(driver.current_url) #печатаем верный сайт
-get_url = driver.current_url
-url = "https://www.saucedemo.com/inventory.html" #необходимый результат
-assert url == get_url #сравниваем необходимый с фактическим
-print("URL верный") #печатаем в вывод
+#print(driver.current_url) #печатаем верный сайт
+#get_url = driver.current_url
+#url = "https://www.saucedemo.com/inventory.html" #необходимый результат
+#assert url == get_url #сравниваем необходимый с фактическим
+#print("URL верный") #печатаем в вывод
+
+
+warning_text = driver.find_element(By.XPATH, "//h3[@data-test='error']") #ищем нужный элемент
+value_warning_text = warning_text.text
+assert (value_warning_text == #сравниваем необходимый с фактическим
+        'Epic sadface: Username and password do not match any user in this service')
+print('Cообщение корректно') #печатаем в вывод
+
+
+error_button = driver.find_element(By.XPATH, "//button[@class='error-button']") #ищем необходимый элемент
+error_button.click() #нажимаем на элемент
+print("click error button") #печатаем в вывод
 
 #переменная для поиска элемента с помощью кастомного XPATH
-text_products = driver.find_element(By.XPATH, "//span[@class='title']")
-print(text_products.text) #печатаем желаемый результат
-assert text_products.text == 'Products' #сравниваем желаемый результат с необходимым
-print('Заголовок верен') #печатаем в вывод
+#text_products = driver.find_element(By.XPATH, "//span[@class='title']")
+#print(text_products.text) #печатаем желаемый результат
+#assert text_products.text == 'Products' #сравниваем желаемый результат с необходимым
+#print('Заголовок верен') #печатаем в вывод
 
 
 #time.sleep(5) #говорим браузеру ничего не делать в течении 5 секунд
